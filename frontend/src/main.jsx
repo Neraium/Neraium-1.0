d9f422389197da18fcd08b72621e790e5038852b
Neraium <neraiumai@gmail.com>
2026-05-13 04:20:40 -0500

    Clean main.jsx style imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./neraium-hardening.css";
-import "./desktop-orb-top.css";
-import "./mobile-restore.css";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
