f92e8dc2711dcddf0ed492f68bb46131babc9dc2
Neraium <neraiumai@gmail.com>
2026-05-13 04:08:27 -0500

    Load mobile responsive restore stylesheet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./neraium-hardening.css";
 import "./desktop-orb-top.css";
+import "./mobile-restore.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
